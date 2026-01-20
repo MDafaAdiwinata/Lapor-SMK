2da701edd3efe7c019a0715a821331c6d97c123a
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-20 21:09:29 +0700

    Tabel Laporan

---
+++ database/migrations/2026_01_20_135555_create_laporan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporan', function (Blueprint $table) {
+            $table->id('id_laporan');
+            $table->string('judul_laporan');
+            $table->text('isi_laporan');
+            $table->date('tgl_laporan');
+            $table->string('image')->nullable();
+            $table->foreignId('id_user')->constrained('users', 'id_user')->onDelete('cascade');
+            $table->foreignId('id_kategori')->constrained('kategori', 'id_kategori')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporan');
+    }
+};
