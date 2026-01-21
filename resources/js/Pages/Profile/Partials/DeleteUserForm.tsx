613985376f39107471f34f73b06df38c338f2f02
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-21 19:23:38 +0700

    Update Profile - Delete Account Card Profile

--- resources/js/Pages/Profile/Partials/DeleteUserForm.tsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.tsx
@@ -52,33 +52,29 @@ export default function DeleteUserForm({
     return (
         <section className={`space-y-6 ${className}`}>
             <header>
-                <h2 className="text-lg font-medium text-gray-900">
-                    Delete Account
-                </h2>
+                <h2 className="text-lg font-medium text-black">Hapus Akun</h2>
 
-                <p className="mt-1 text-sm text-gray-600">
-                    Once your account is deleted, all of its resources and data
-                    will be permanently deleted. Before deleting your account,
-                    please download any data or information that you wish to
-                    retain.
+                <p className="mt-1 text-sm md:text-md text-black/80 font-light">
+                    Setelah akun dihapus, seluruh data akan hilang secara
+                    permanen. Pastikan Anda telah menyimpan data penting sebelum
+                    melanjutkan.
                 </p>
             </header>
 
             <DangerButton onClick={confirmUserDeletion}>
-                Delete Account
+                Hapus Akun
             </DangerButton>
 
             <Modal show={confirmingUserDeletion} onClose={closeModal}>
                 <form onSubmit={deleteUser} className="p-6">
                     <h2 className="text-lg font-medium text-gray-900">
-                        Are you sure you want to delete your account?
+                        Apakah Anda yakin ingin menghapus akun Anda?{" "}
                     </h2>
 
                     <p className="mt-1 text-sm text-gray-600">
-                        Once your account is deleted, all of its resources and
-                        data will be permanently deleted. Please enter your
-                        password to confirm you would like to permanently delete
-                        your account.
+                        Setelah akun Anda dihapus, semua data akan dihapus
+                        secara permanen. Silakan masukkan kata sandi Anda untuk
+                        mengkonfirmasi penghapusan akun.
                     </p>
 
                     <div className="mt-6">
@@ -95,7 +91,7 @@ export default function DeleteUserForm({
                             ref={passwordInput}
                             value={data.password}
                             onChange={(e) =>
-                                setData('password', e.target.value)
+                                setData("password", e.target.value)
                             }
                             className="mt-1 block w-3/4"
                             isFocused
@@ -110,11 +106,11 @@ export default function DeleteUserForm({
 
                     <div className="mt-6 flex justify-end">
                         <SecondaryButton onClick={closeModal}>
-                            Cancel
+                            Batal
                         </SecondaryButton>
 
-                        <DangerButton className="ms-3" disabled={processing}>
-                            Delete Account
+                        <DangerButton className="ms-2" disabled={processing}>
+                            Hapus
                         </DangerButton>
                     </div>
                 </form>
