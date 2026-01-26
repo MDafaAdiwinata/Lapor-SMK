b3e02af12955d6766b9a783f50bbfb9f8cefa294
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-26 14:27:43 +0700

    Vite Server Config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: "192.168.1.9",
-            protocol: "ws",
-        },
-    },
-});
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: "resources/js/app.tsx",
+                refresh: true,
+            }),
+            react(),
+        ],
+        // server: {
+        //     host: env.VITE_DEV_HOST || "0.0.0.0",
+        //     port: Number(env.VITE_DEV_PORT) || 5173,
+        //     strictPort: true,
+        //     hmr: {
+        //         host: env.VITE_HMR_HOST,
+        //         protocol: env.VITE_HMR_PROTOCOL || "ws",
+        //     },
+        // },
+    });
+};
