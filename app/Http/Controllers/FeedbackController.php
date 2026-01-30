1a41d5a8530a5ba1ed3be67c552f117c61250448
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-30 20:20:00 +0700

    feat: DB BE Setup - Migration Table, Model, Controller Index

---
+++ app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Feedback;
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $feedbacks = Feedback::lazy();
+        return Inertia::render('Admin/Feedbacks/Index', compact('feedbacks'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
